4e937d1c1016ddd4afdf3e0d32aabc825664efaf
willian-rodrigues <willian.rsilva1994@gmail.com>
2022-07-13 21:13:43 -0300

    feat(App/Base): configuração inicial do projeto, com estrutura e principais libs

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home | eCrypto</title>
+      </Head>
+      <h1>Coxinha</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/styles/global.styles.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    cursor: arrow;
+  }
+  body,
+  input,
+  textarea,
+  select,
+  button {
+    font: 400 1rem 'Poppins', sans-serif;
+  }
+  button {
+    cursor: pointer;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }  
+`;
